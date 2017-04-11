1722996d08ecc03ea542d0c5b5d1dcea0145ced0
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-11 20:11:57 +0300

    feat(templates): add templates task

---
+++ gulpfile.js/tasks/templates.js
@@ -0,0 +1,35 @@
+const gulp          = require('gulp')
+const pug           = require('gulp-pug')
+const plumber       = require('gulp-plumber')
+const errorHandler  = require('gulp-plumber-error-handler')
+const cached        = require('gulp-cached')
+const prettify      = require('gulp-jsbeautifier')
+const filter        = require('gulp-filter')
+const rename        = require('gulp-rename')
+
+// TODO add pug inheritance in watch mode
+// TODO add static hash to assets in production mode
+
+gulp.task('templates', () => (
+  gulp.src('app/**/*.pug')
+    .pipe(plumber({ errorHandler: errorHandler('Error in templates task') }))
+    .pipe(cached('pug'))
+    .pipe(filter(file => /app[\\\/]pages/.test(file.path)))
+    .pipe(pug())
+    .pipe(prettify({
+      braceStyle: 'expand',
+      indentWithTabs: true,
+      indentInnerHtml: true,
+      preserveNewlines: true,
+      endWithNewline: true,
+      wrapLineLength: 120,
+      maxPreserveNewlines: 50,
+      wrapAttributesIndentSize: 1,
+      unformatted: ['use']
+    }))
+    .pipe(rename({ dirname: '.' }))
+    .pipe(gulp.dest('dist'))
+))
+
+
+
