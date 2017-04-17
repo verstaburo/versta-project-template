9a768bdac8a7e73b2d9da82d0ee3c85ad1359822
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-04-17 16:17:47 +0300

    fix(styles): fix styles task

--- gulpfile.js/tasks/styles.js
+++ gulpfile.js/tasks/styles.js
@@ -38,7 +38,7 @@ gulp.task('styles', () => {
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist/assets/styles'));
 
-  // gulp.start('styles:lint');
+  gulp.start('styles:lint');
 });
 
 gulp.task('styles:lint', () => (
