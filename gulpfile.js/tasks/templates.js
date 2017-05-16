5693b995fde8b17d20368f8154f6b3685ca47b94
Alexey Bogoslovskiy <bogoslovskiya@gmail.com>
2017-05-16 21:29:33 +0300

    fix(templates): https://github.com/verstaburo/versta-project-template/issues/5

--- gulpfile.js/tasks/templates.js
+++ gulpfile.js/tasks/templates.js
@@ -2,24 +2,17 @@ const gulp = require('gulp');
 const pug = require('gulp-pug');
 const plumber = require('gulp-plumber');
 const errorHandler = require('gulp-plumber-error-handler');
-const cached = require('gulp-cached');
 const prettify = require('gulp-jsbeautifier');
 const filter = require('gulp-filter');
 const rename = require('gulp-rename');
-const pugInheritance  = require('gulp-pug-inheritance');
-const changed = require('gulp-changed');
+const pugInheritance = require('gulp-pug-inheritance');
+const gulpIf = require('gulp-if');
 const getData = require('jade-get-data')('app/data');
-
-// TODO add static hash to assets in production mode
 
 gulp.task('templates', () => (
   gulp.src('app/**/*.pug')
     .pipe(plumber({ errorHandler: errorHandler('Error in templates task') }))
-    .pipe(changed('dist', { extension: '.html' }))
-    .pipe(pugInheritance({
-      basedir: 'app/pages',
-      extension: '.pug',
-    }))
+    .pipe(gulpIf(global.isWatching, pugInheritance({ basedir: 'app' })))
     .pipe(filter(file => /app[\\\/]pages/.test(file.path))) // eslint-disable-line no-useless-escape
     .pipe(pug({ data: { getData } }))
     .pipe(prettify({
