c6696fc44c86dae41aa9ea2bbff2bc632ac0d65c
Gleb Gorokhov <gg@verstaburo.com>
2017-09-18 19:16:48 +0300

    global update

---
+++ app/blocks/js-functions/anchor.js
@@ -0,0 +1,15 @@
+// При клике на .js-anchor страница плавно скроллится к блоку, указанному в его href
+
+const $ = window.$;
+
+export default function anchor() {
+  $(document).on('click', '.js-anchor', function (e) {
+    e.preventDefault();
+    const target = $($(this).attr('href'));
+    if (target.length > 0) {
+      $('body, html').animate({
+        scrollTop: target.offset().top,
+      }, 1000, 'swing');
+    }
+  });
+}
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,13 @@
 import svg4everybody from 'svg4everybody';
 import objectFitImages from 'object-fit-images';
+import anchor from '../blocks/js-functions/anchor';
+import { freezebuttons } from '../blocks/js-functions/freeze';
 
 const $ = window.$;
 
 $(() => {
   svg4everybody();
   objectFitImages();
+  anchor();
+  freezebuttons();
 });
