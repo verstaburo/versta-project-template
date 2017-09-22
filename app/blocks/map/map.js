7ee930dc2d1781c3c6d590c1c58c42998cc61ff3
Gleb Gorokhov <gg@verstaburo.com>
2017-09-22 15:32:12 +0300

    map fix

--- app/blocks/map/map.js
+++ app/blocks/map/map.js
@@ -6,7 +6,7 @@ import L from 'leaflet';
 const $ = window.$;
 
 export default function maps() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([59.934, 30.335], 13);
 
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     maxZoom: 18,
@@ -14,8 +14,8 @@ export default function maps() {
     accessToken: 'pk.eyJ1IjoidGhldmVydmVyeTEiLCJhIjoiY2lzZXdzaXZ4MDBjaTJudm93dDI4MGVrMCJ9.Z8KKk0M_lpDTPB6_JtJBxg',
   }).addTo(map);
 
-  const marker = L.marker([51.5, -0.09]).addTo(map);
+  const marker = L.marker([59.934, 30.335]).addTo(map);
 
-  marker.bindPopup('<b>Hello world!</b><br>I am a popup.');
+  marker.bindPopup('Кастомный HTML-попап');
 }
 /* eslint-enable no-unused-vars */
