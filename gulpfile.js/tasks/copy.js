0cf32319d2616b37e905982b708c76f8eb70dcbf
Алексей Богословский <bogoslovskiya@gmail.com>
2018-01-18 12:41:13 +0300

    copy dot files to misc

--- gulpfile.js/tasks/copy.js
+++ gulpfile.js/tasks/copy.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const newer = require('gulp-newer');
 
+const miscPath = ['app/static/misc/**/*', 'app/static/misc/.*'];
+
 module.exports = () => (
-    gulp
-    .src('app/static/misc/**/*', { since: gulp.lastRun('copy') })
+  gulp
+    .src(miscPath, { since: gulp.lastRun('copy') })
     .pipe(newer('dist'))
     .pipe(gulp.dest('dist'))
 );
