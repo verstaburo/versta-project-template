7e4e1fa931c2af1adb76278a9bb5aa877368a65e
Алексей Богословский <bogoslovskiya@gmail.com>
2018-01-28 15:35:07 +0300

    fix accordion

--- app/blocks/accordion/accordion.js
+++ app/blocks/accordion/accordion.js
@@ -1,5 +1,9 @@
-export const HIDE_EVENT = 'accordion.show';
-export const SHOW_EVENT = 'accordion.hide';
+export const HIDDEN = 'accordion/shown';
+export const SHOWN = 'accordion/hidden';
+export const BEFORE_SHOW = 'accordion/beforeshow';
+export const BEFORE_HIDE = 'accordion/beforehide';
+
+const DURATION = 250;
 
 const $ = window.$;
 
@@ -8,14 +12,32 @@ $(document).on('click', '.js-accordion-button', function (e) {
   const button = $(this);
   const block = button.parents('.accordion');
   const body = block.find('.accordion__body');
+  const isActive = Number(block.hasClass('is-active'));
+  const isMultiple = block.parents('.accordions').data('accordion-multiple');
 
   if (block.hasClass('is-disabled')) {
     return;
   }
 
-  body.slideToggle(250, () => {
-    block.toggleClass('is-active');
-    const event = [HIDE_EVENT, SHOW_EVENT][+block.hasClass('is-active')];
-    block.trigger(event);
+  const beforeEvent = [BEFORE_SHOW, BEFORE_HIDE][isActive];
+  const afterEvent = [SHOWN, HIDDEN][isActive];
+
+  body.trigger(beforeEvent).slideToggle(DURATION, () => {
+    block
+      .toggleClass('is-active')
+      .trigger(afterEvent);
   });
+
+  if (!isMultiple) {
+    const siblings = block.siblings('.accordion.is-active');
+
+    siblings
+      .trigger(BEFORE_HIDE)
+      .find('.accordion__body')
+      .slideUp(DURATION, () => {
+        siblings
+          .removeClass('is-active')
+          .trigger(HIDDEN);
+      });
+  }
 });
